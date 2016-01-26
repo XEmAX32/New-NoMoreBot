cbb00660bc950176fd1e2e16dbca6442e1c6e383
XEmAX32 <XEmAX32@gmail.com>
2016-01-26 22:15:56 +0100

    Update Main.php

--- src/NoMoreBot/Main.php
+++ src/NoMoreBot/Main.php
@@ -6,4 +6,28 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
 class Main extends PluginBase implements Listener{
+  
+  private $whitelist == false;
+  private $IPs = [];
+  
+public function onEnable(){
+  $this->getServer()->PluginManager()->registerEvents($this, $this);
+  $this->getLogger->info(TextFormat::BLUE . "[NoMoreBot Enabled]");
+  $this->saveDefaultConfig();
+  $this->wl = new Config($this->getDataFolder()."NMBWhitelist.properties", Config::PROPERTIES);
+}
+
+public onLoad(){
+  $this->getLogger->info(TextFormat::BLUE . "[NoMoreBot Loading]");
+  $wl->save();
+}
+
+public function onDisable(){
+  $this->getLogger()->info(TextFormat::BLUE . "[NoMoreBot Disabled]");
+  $wl->save();
+  }
+
+public function onPreSpawn(PlayerPreSpawnEvent $e){
+  //tomorrow big commit!
+}
 }
